5df193d54fdbb2311a8a0621a679772f7fb020c0
gurumurthi97 <guru13041997@gmail.com>
2022-10-12 16:33:38 +0530

    fetch the data of name and symbol

--- src/components/DashboardComponents/Grid/index.js
+++ src/components/DashboardComponents/Grid/index.js
@@ -1,1 +1,17 @@
+import React from "react";
+import "./styles.css";
+function Grid({ coin }) {
+  return (
+    <div className="coin-box">
+      <div className="logo-div">
+        <img className="logo" src={coin.image} />
+        <div className="coin-info">
+          <p className="symbol">{coin.symbol}</p>
+          <p className="name">{coin.name}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
 
+export default Grid;
