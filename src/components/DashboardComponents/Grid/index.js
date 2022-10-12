5cc32b7d9df0311f132ecb119c129463a232313d
gurumurthi97 <guru13041997@gmail.com>
2022-10-12 17:00:28 +0530

    added data

--- src/components/DashboardComponents/Grid/index.js
+++ src/components/DashboardComponents/Grid/index.js
@@ -1,3 +1,4 @@
+import { green } from "@mui/material/colors";
 import React from "react";
 import "./styles.css";
 function Grid({ coin }) {
@@ -6,8 +7,27 @@ function Grid({ coin }) {
       <div className="logo-div">
         <img className="logo" src={coin.image} />
         <div className="coin-info">
-          <p className="symbol">{coin.symbol}</p>
+          <p className="symbol">{coin.symbol}-USD</p>
           <p className="name">{coin.name}</p>
+        </div>
+      </div>
+      <div className="data-div">
+        <div
+          className="chip"
+          style={{
+            color:
+              coin.price_change_percentage_24h > 0
+                ? "var(--green)"
+                : "var(--red)",
+            borderColor:
+              coin.price_change_percentage_24h > 0
+                ? "var(--green)"
+                : "var(--red)",
+          }}
+        >
+          {coin.price_change_percentage_24h > 0
+            ? "+" + coin.price_change_percentage_24h
+            : coin.price_change_percentage_24h}
         </div>
       </div>
     </div>
